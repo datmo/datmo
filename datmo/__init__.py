c001278558a2cb098cf402249c670c61692ad724
Anand Sampat <asampat3090@gmail.com>
2018-04-18 22:39:53 -0700

    load in modules in __init__.py so "import datmo" works for the user

---
+++ datmo/__init__.py
@@ -0,0 +1,8 @@
+"""
+Bring in all of Datmo's public interfaces
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import datmo.snapshot
