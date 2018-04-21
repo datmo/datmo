8215e542b351ed754aa7a5267a047bc3c07eb822
Shabaz Patel <shabaz.b.patel@gmail.com>
2018-04-20 18:18:12 -0700

    adding test for snapshot ls client

--- datmo/test/test_snapshot.py
+++ datmo/test/test_snapshot.py
@@ -5,7 +5,7 @@ import os
 import shutil
 import tempfile
 
-from datmo.snapshot import create
+from datmo.snapshot import create, ls
 from datmo.core.controller.project import ProjectController
 from datmo.core.util.exceptions import GitCommitDoesNotExist, \
     DoesNotExistException
@@ -35,7 +35,6 @@ class TestSnapshotModule():
         assert failed
 
         # Create a snapshot with default params and files to commit
-        # (fails w/ no environment)
         test_filepath = os.path.join(self.temp_dir, "script.py")
         with open(test_filepath, "w") as f:
             f.write("import numpy\n")
@@ -65,3 +64,27 @@ class TestSnapshotModule():
         assert snapshot_obj_2.config == {}
         assert snapshot_obj_2.stats == {}
         assert snapshot_obj_2 != snapshot_obj_1
+
+    def test_ls(self):
+
+        # create with default params and files to commit
+        test_filepath = os.path.join(self.temp_dir, "script.py")
+        with open(test_filepath, "w") as f:
+            f.write("import numpy\n")
+            f.write("import sklean\n")
+
+        create(message="test", home=self.temp_dir)
+
+        # Create a snapshot with default params, files, and environment
+        test_filepath = os.path.join(self.temp_dir, "Dockerfile")
+        with open(test_filepath, "w") as f:
+            f.write("FROM datmo/xgboost:cpu")
+        create(message="test", home=self.temp_dir)
+
+        # list all snapshots with no filters
+        snapshot_ls_1 = ls(home=self.temp_dir)
+        # list snapshots with specific filter
+        snapshot_ls_2 = ls(filter='test', home=self.temp_dir)
+        assert snapshot_ls_1
+        assert len(snapshot_ls_1) == 2
+        assert len(snapshot_ls_2) == 2
