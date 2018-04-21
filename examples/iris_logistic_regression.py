cebe39fdc2d9644c52d25922491c31f989681fc7
Nicholas Walsh <nicholaswalsh.official@gmail.com>
2018-04-20 17:29:51 -0700

    Add iris example for snapshot creation in python script and jupyter notebook

---
+++ examples/iris_logistic_regression.py
@@ -0,0 +1,19 @@
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import datmo # extra line
+
+config = { "solver": "newton-cg" } # extra line
+iris_dataset = datasets.load_iris()
+X, y = iris_dataset.data, iris_dataset.target
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+model = LogisticRegression(**config).fit(X_train, y_train)
+
+train_acc = model.score(X_train, y_train)
+test_acc = model.score(X_test, y_test)
+
+print(train_acc)
+print(test_acc)
+
+stats = { "train_accuracy": train_acc, "test_accuracy": test_acc } # extra line
+datmo.snapshot.create(config=config, stats=stats) # extra line
