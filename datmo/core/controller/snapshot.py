d814c599b6ec03a1bbc195522aa3a5f0dad5450a
Arron <arron@acusense.ai>
2018-04-24 16:51:21 -0700

    Use double underscore

--- datmo/core/controller/snapshot.py
+++ datmo/core/controller/snapshot.py
@@ -36,7 +36,7 @@ class SnapshotController(BaseController):
         Delete the snapshot specified from the project
 
     """
-    def __init__(self, home):
+    def ___init__(self, home):
         super(SnapshotController, self).__init__(home)
         self.code = CodeController(home)
         self.file_collection = FileCollectionController(home)
@@ -149,19 +149,19 @@ class SnapshotController(BaseController):
         }
 
         # Code setup
-        self._code_setup(incoming_dictionary, create_dict)
+        self.__code_setup(incoming_dictionary, create_dict)
 
         # Environment setup
-        self._env_setup(incoming_dictionary, create_dict)
+        self.__env_setup(incoming_dictionary, create_dict)
 
         # File setup
-        self._file_setup(incoming_dictionary, create_dict)
+        self.__file_setup(incoming_dictionary, create_dict)
 
         # Config setup
-        self._config_setup(incoming_dictionary, create_dict)
+        self.__config_setup(incoming_dictionary, create_dict)
 
         # Stats setup
-        self._stats_setup(incoming_dictionary, create_dict)
+        self.__stats_setup(incoming_dictionary, create_dict)
 
         # If snapshot object with required args already exists, return it
         # DO NOT create a new snapshot with the same required arguments
@@ -223,7 +223,7 @@ class SnapshotController(BaseController):
                                             "id"))
         return self.dal.snapshot.delete(id)
 
-    def _config_setup(self, incoming_dictionary, create_dict):
+    def __config_setup(self, incoming_dictionary, create_dict):
         """[summary]
             Fills in snapshot config by having one of the following:
             1. config = JSON object
@@ -249,9 +249,9 @@ class SnapshotController(BaseController):
         else:
             config_filename = incoming_dictionary['config_filename'] \
                 if "config_filename" in incoming_dictionary else "config.json"
-            create_dict['config'] = self._find_in_filecollection(config_filename, create_dict['file_collection_id'])
+            create_dict['config'] = self.__find_in_filecollection(config_filename, create_dict['file_collection_id'])
 
-    def _stats_setup(self, incoming_dictionary, create_dict):
+    def __stats_setup(self, incoming_dictionary, create_dict):
         """[summary]
             Fills in snapshot stats by having one of the following:
             1. stats = JSON object
@@ -278,9 +278,9 @@ class SnapshotController(BaseController):
         else:
             stats_filename = incoming_dictionary['stats_filename'] \
                 if "stats_filename" in incoming_dictionary else "stats.json"
-            create_dict['stats'] = self._find_in_filecollection(stats_filename, create_dict['file_collection_id'])
+            create_dict['stats'] = self.__find_in_filecollection(stats_filename, create_dict['file_collection_id'])
 
-    def _file_setup(self, incoming_dictionary, create_dict):
+    def __file_setup(self, incoming_dictionary, create_dict):
         """ TODO:
 
         Arguments:
@@ -299,7 +299,7 @@ class SnapshotController(BaseController):
             create_dict['file_collection_id'] = self.file_collection.\
                 create([]).id
 
-    def _env_setup(self, incoming_dictionary, create_dict):
+    def __env_setup(self, incoming_dictionary, create_dict):
         """ TODO:
 
         Arguments:
@@ -322,7 +322,7 @@ class SnapshotController(BaseController):
             create_dict['environment_id'] = self.environment.\
                 create({}).id
 
-    def _code_setup(self, incoming_dictionary, create_dict):
+    def __code_setup(self, incoming_dictionary, create_dict):
         """ Set the code_id by using:
             1. code_id
             2. commit_id string, which creates a new code_id
@@ -341,7 +341,7 @@ class SnapshotController(BaseController):
         else:
             create_dict['code_id'] = self.code.create().id
 
-    def _find_in_filecollection(self, file_to_find, file_collection_id):
+    def __find_in_filecollection(self, file_to_find, file_collection_id):
         """ Attempts to find a file within the file collection
 
         Arguments:
