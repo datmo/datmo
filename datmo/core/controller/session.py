acef41cceb4ad17e98ab3b55a9072a26dd7d7465
Shabaz Patel <shabaz.b.patel@gmail.com>
2018-05-03 01:02:36 -0700

    adding sort to session list in controller

--- datmo/core/controller/session.py
+++ datmo/core/controller/session.py
@@ -86,8 +86,8 @@ class SessionController(BaseController):
             "current": True
         })
 
-    def list(self, query = {}):
-        return self.dal.session.query(query)
+    def list(self, query = {}, sort_key=None, sort_order=None):
+        return self.dal.session.query(query, sort_key, sort_order)
 
     def delete_by_name(self, name):
         if name == 'default':
