15e73f982e4042da99dfaf9bb340bbb71ca9ebc9
Anand Sampat <asampat3090@gmail.com>
2018-05-05 10:39:35 -0700

    move version to separate file to prevent circular dependencies

--- datmo/__init__.py
+++ datmo/__init__.py
@@ -4,9 +4,10 @@ Bring in all of Datmo's public python interfaces
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+from version import __version__
+
+__version__ = __version__
 
 import datmo.snapshot
 import datmo.task
 import datmo.config
-
-__version__ = '0.0.2-dev'
--- docs/conf.py
+++ docs/conf.py
@@ -14,16 +14,15 @@
 
 import os
 import sys
-import recommonmark
 from recommonmark.transform import AutoStructify
-
-from datmo import __version__
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
 sys.path.insert(0, os.path.abspath("../"))
+
+from version import __version__
 
 # -- General configuration ------------------------------------------------
 
