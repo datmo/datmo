71187772b62b2a635ff7c2320a293f2c23910487
Anand Sampat <asampat3090@gmail.com>
2018-05-05 12:09:11 -0700

    add `datmo version`, `datmo --version`, `datmo -v` commands for versions + moved to common VERSION file included in datmo package build

--- datmo/cli/command/tests/test_project.py
+++ datmo/cli/command/tests/test_project.py
@@ -16,6 +16,7 @@ import os
 import tempfile
 import platform
 
+from datmo import __version__
 from datmo.cli.driver.helper import Helper
 from datmo.cli.parser import parser
 from datmo.cli.command.project import ProjectCommand
@@ -30,22 +31,36 @@ class TestProject():
                                         tempfile.gettempdir())
         self.temp_dir = tempfile.mkdtemp(dir=test_datmo_dir)
         self.cli_helper = Helper()
-        self.init = ProjectCommand(self.temp_dir, self.cli_helper, parser)
+        self.project = ProjectCommand(self.temp_dir, self.cli_helper, parser)
 
     def teardown_class(self):
         pass
 
     def test_datmo_init(self):
-        self.init.parse(
+        self.project.parse(
             ["init", "--name", "foobar", "--description", "test model"])
-        self.init.execute()
+        self.project.execute()
         # test for desired side effects
         assert os.path.exists(os.path.join(self.temp_dir, '.datmo'))
 
     def test_datmo_init_invalid_arg(self):
         exception_thrown = False
         try:
-            self.init.parse(["init", "--foobar", "foobar"])
+            self.project.parse(["init", "--foobar", "foobar"])
+        except Exception:
+            exception_thrown = True
+        assert exception_thrown
+
+    def test_datmo_version(self):
+        self.project.parse(["version"])
+        result = self.project.execute()
+        # test for desired side effects
+        assert __version__ in result
+
+    def test_datmo_version_invalid_arg(self):
+        exception_thrown = False
+        try:
+            self.project.parse(["version", "--foobar"])
         except Exception:
             exception_thrown = True
         assert exception_thrown
