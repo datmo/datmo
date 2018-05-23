1d5e4cf9ab0ae2da3bc6e7ac0db778a955deacc7
Nicholas Walsh <nicholaswalsh.official@gmail.com>
2018-05-23 16:29:29 -0700

    Change config and stats preprocessing to allow for multiple values of each to be passed in by user

--- examples/R/snapshot-create-iris-caret.R
+++ examples/R/snapshot-create-iris-caret.R
@@ -1,34 +1,28 @@
-## Import data
 library(datasets)
+library(caret)
+library(rpart.plot)
 
+## Import data
 df <- iris
 head(df) # View first few rows of dataset
 
 ## Fit Model
-library(caret)
-
 modFit <- train(Species ~., method = "rpart", data=df) #Fit model
-print(modFit$finalModel)   #Summarize model text table)
+print(modFit$finalModel)   #Summarize model
 
 # Visualize Results
-library(rpart.plot)
 rpart.plot(modFit$finalModel) #create decision tree visualization
 
-#define model configuration to save
+## Define the properties we want to log in the snapshot
+# Define configuration to save
 config<- paste(sep="",
-  "method:", modFit$method)
- # "modelType"= modFit$modelType)
+               " --config method:", modFit$method,
+               " --config modelType:", modFit$modelType)
 
-#define metrics to save
+#define metrics to save from the model
 stats<- paste(sep="",
-  "Accuracy:", modFit$results$Accuracy[1])
-  #"Kappa"= modFit$results$Kappa[1]) 
+              " --stats Accuracy:", modFit$results$Accuracy[1],
+              " --stats Kappa:", modFit$results$Kappa[1])
 
 # Create snapshot with configuration and stats
-system2("datmo", 
-        args=paste("snapshot create", 
-          "-m 'a full snapshot'", 
-          '--config', config,
-          '--stats', stats))
-# system2("datmo", args=paste("snapshot create -m 'an inline snapshot' --config ", config, " --stats", stats)  
-# system2("datmo", args="snapshot create -m 'an inline snapshot' ") #snapshot create, no metrics, no stats
+system2("datmo", args=paste("snapshot create", "-m 'Whoah, my first snapshot!'", config, stats))
